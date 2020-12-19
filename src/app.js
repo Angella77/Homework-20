81a8cdec8ec118087cd3de29c9c8b92dd9dd90fa
Angella77 <angiejeanlewis@gmail.com>
2020-12-19 00:18:47 -0500

    add changes

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import AboutMe from "./components/pages/AboutMe";
+import Portfolio from "./components/pages/Portfolio";
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <div>
+        <NavTabs />
+        <Route exact path="/" component={AboutMe} />
+        <Route exact path="/Portfolio" component={Portfolio}/>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
